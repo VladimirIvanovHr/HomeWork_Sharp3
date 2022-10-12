4b6d46592154985389a934f193c3d9d4513ed707
Vladimir Ivanov <ivanofvv9@gmail.com>
2022-10-13 01:20:55 +0300

    Задача на нахождение расстояния куба

---
+++ HW_3_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число\tКуб");
+for (int i=1; i<(number + 1); i++)
+    {   
+        Console.WriteLine( "{0}\t{1}\t", i, (Math.Pow(i,3)));
+    }
